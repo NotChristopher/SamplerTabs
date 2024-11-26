9473b503c88f46027255330bec8929fd58c1d4a1
Christopher Brummer <64840908+NotChristopher@users.noreply.github.com>
2024-11-27 00:27:50 +0200

    update

--- SamplerTabs/app/index.tsx
+++ SamplerTabs/app/index.tsx
@@ -1,6 +1,7 @@
 import { Text, View } from "react-native";
 import PadsSection from "@/components/ui/PadsSection";
 import TopController from "@/components/ui/TopController";
+
 export default function Index() {
   return (
     <View
